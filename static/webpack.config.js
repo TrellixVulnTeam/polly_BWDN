e5d9b1b0163299c607fc1e5cc7f1fefa36600b51
Guy <glosica3@gmail.com>
2017-03-30 18:49:44 +0300

    added client

---
+++ static/webpack.config.js
@@ -0,0 +1,45 @@
+var path = require("path")
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+  debug: true,
+  context: __dirname,
+  devtool: 'source-map',
+
+  // entry point of our app. static/js/index.js should require other js modules and dependencies it needs
+  entry: './src/index.js', 
+
+  output: {
+      path: path.resolve('./bundles/'),
+      filename: "[name].js",
+      publicPath: '/bundles/'
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { // to transform JSX into JS
+        test: /\.jsx?$/,         // Match both .js and .jsx files
+        exclude: /node_modules/, 
+        loader: "babel-loader", 
+        query: {
+          presets:['react', 'es2015']
+        }
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'file-loader' },
+    ],
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+    extensions: ['', '.js', '.jsx']
+  },
+}
